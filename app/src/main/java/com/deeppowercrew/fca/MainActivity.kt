77a2310ff021666223be6eefa1dfdc8201f9d882
DT5GEN <alex-shilov@yandex.ru>
2022-08-26 04:50:44 +0400

    fix conflict

---
+++ app/src/main/java/com/deeppowercrew/fca/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.deeppowercrew.fca
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deeppowercrew.fca.ui.theme.FirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+
