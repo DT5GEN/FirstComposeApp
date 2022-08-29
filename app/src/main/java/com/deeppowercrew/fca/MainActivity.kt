c0171eb95e996da918ea4dca9bcb334a77d9ce7e
DT5GEN <alex-shilov@yandex.ru>
2022-08-29 04:05:03 +0300

    added scroll & longpress-drag listener

--- app/src/main/java/com/deeppowercrew/fca/MainActivity.kt
+++ app/src/main/java/com/deeppowercrew/fca/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.deeppowercrew.fca
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,7 +20,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -25,7 +30,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                 listItem(name = "Добромирчик", prof = " Умняшка ")
                 listItem(name = "Добромирчик", prof = " Умняшка ")
                 listItem(name = "Добромирчик", prof = " Умняшка ")
@@ -46,7 +51,16 @@ private fun listItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d(TAG, "YYY listItem() called with: change = $change, dragAmount = $dragAmount")
+                }
+            }
+            .padding(10.dp)
+            .offset(20.dp)
+            .clickable {
+                Log.d(TAG, "XXX listItem() clicked")
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 7.dp
 
